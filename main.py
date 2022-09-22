aa9021ac7e289d3eb2eabfd56f1d40dd6898ea5c
Pranay110201 <97798992+Pranay110201@users.noreply.github.com>
2022-09-22 17:52:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 
 def count_batteries_by_usage(cycles):
+#   Variables to keep count of various categories of batteries
   lowcount=0
   mediumcount=0
   highcount=0
+#   loop to iterate through the input values present in cycles
   for i in cycles:
+#     condition to check and categorize them based of the cycles 
     if(i<410):
       lowcount+=1
     elif(i>=410 and i<909):
       mediumcount+=1
     else:
       highcount+=1
+#    returning the count of each catergories of the batteries.
   return {
     "lowCount": lowcount,
     "mediumCount": mediumcount,
